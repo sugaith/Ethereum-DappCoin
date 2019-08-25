5c1a5480c3f6382b24abdf67b805461f92340791
thiago <sugaith@gmail.com>
2019-08-25 19:02:13 -0300

    Sales contract for ICO COMPLETED

--- contracts/SugEtherSale.sol
+++ contracts/SugEtherSale.sol
@@ -2,9 +2,20 @@ pragma solidity >=0.4.21 <0.6.0;
 
 import "./Dapptoken.sol" ;
 
+//todo
+// PROVISION TOKENS TO TOKEN SALE CONTRACT  //FORNECER TOKENS PARA VENDA
+// SET A TOKEN PRICE IN WEI //SETAR UM PREÇO DE TOKENS EM WEI
+// ASSING AN ADMIN //SETAR UM ADMIN (MSG.SENDER)
+// BUY TOKENS //FUNCAO DE COMPRA DE TOKENS
+// END SALE //FUNCVAO DE TERMINO DE VENDA ICO
+
+
+
 contract SugEtherSale{
     //admin address: private
-    address admin;
+    //person ro deployed contract or colled the function
+    //truffle LENDS an account qhen the contract is deployed
+    address payable admin;
 
     //token for token contract (SugEther)
     DappToken public tokenContract;
@@ -12,8 +23,16 @@ contract SugEtherSale{
     //token price
     uint256 public tokenPrice;
 
+    //tokens sold / vendidos
+    uint256 public tokensSold;
+
+    //SellEvent
+    event Sell(address _buyer, uint256 _amount);
+
+
     constructor( DappToken _tokenContract, uint256 _tokenPrice ) public {
         //assign an admin
+        //the guy who deployed the contract or who is selling tokens
         admin = msg.sender;
 
         // token contract
@@ -23,5 +42,74 @@ contract SugEtherSale{
         tokenPrice = _tokenPrice;
     }
 
+    //function to buy tokens
+    //payable -->
+    function buyTokens(uint256 _numbOfTokens) public payable {
+        //require if valueis equal to tokens
+        //confere se valor é igual a tokens
+//        require( msg.value == _numbOfTokens * tokenPrice );//OR
+        require( msg.value == multiply( _numbOfTokens, tokenPrice ));
+
+        //require if there is enough tokens
+        //confere se tem tokens disponiveis
+        //this -> solidity shortcut
+        require( tokenContract.balanceOf( address(this) ) >= _numbOfTokens );
+
+        //BUY FUNCTIONALITY
+        //require if transfer is successfull
+        //confere se a transferencia é bem sucedida
+        require( tokenContract.transfer(msg.sender, _numbOfTokens) );
+
+
+        //keep track of number of tokens old
+        //manter historico dos tokens vendidos
+
+
+        tokensSold += _numbOfTokens;
+
+        //trigger sell event
+        //dispara evento de VEnda
+        emit Sell(msg.sender, _numbOfTokens);
+    }
+
+    //END SALE
+    function endSale() public   {
+        //require if ender is the admin
+        require( msg.sender == admin );
+
+        //transfer balance back to admin
+        require( tokenContract.transfer(admin, tokenContract.balanceOf( address(this) )) );
+
+        //destructsale contrat
+    //https://solidity.readthedocs.io/en/v0.5.10/units-and-global-variables.html#contract-related
+        //NOTE:
+        /*
+        A word of caution. selfdestruct is an inelegant way to stop a contract and could lead to non-trivial problems.
+         Consider using Open Zeppelin Pausable.sol instead.
+https://ethereum.stackexchange.com/questions/65872/invalid-type-for-argument-in-function-call-invalid-implicit-conversion-from-add
+        */
+        selfdestruct( admin );
+
+
+    }
+
+    //multiply function
+    //its a best practice NOT TO USE '*' operator
+    //using function ds-math/math.sol is safer with requires
+    //https://github.com/dapphub/ds-math/blob/master/src/math.sol
+    //** internal == private ajva
+    //pure -> not writing data to blockchain, just pure function
+    function multiply(uint x, uint y) internal pure returns (uint z){
+        require(y == 0 || (z = x * y) / y == x, "ds-math-mul-overflow");
+    }
+
+
+
+
+
+
+
+
 
 }
+
--- test/SugEtherSale.js
+++ test/SugEtherSale.js
@@ -1,24 +1,105 @@
 var SugEtherSale = artifacts.require("./SugEtherSale.sol");
+var DappToken = artifacts.require("./DappToken.sol");
 
 contract ('SugEtherSale', function ( accounts ) {
-    var saleInstance;
-    var tokenPrice = 1000000000000000; // in WEI / or 0,001 ether
+    let saleInstance;
+    let tokenInstance;
+    let tokenPrice = 1000000000000000; // in WEI / or 0,001 ether
+    let buyer = accounts[1];
+    let admin = accounts[0];
+    //provision 75% of totalSupply
+    //fornecer 75% do totalsupply para o ICO
+    let tokensAvailable = 750000;
 
-    it('::: should initialize sale contract :::', function () {
+
+    it('::: should initiate sale contract :::', function () {
         return SugEtherSale.deployed().then(function (inst) {
             saleInstance = inst;
             return saleInstance.address;
         }).then(function (address) {
-            assert.notEqual(address, 0x0, '-- not admin adress');
+            //todo aqui cada vez me retorna um endereco diferente que nao tem no ganache!
+            // q endereço é este (msg.sender quem é?)???
+            // responde: this address is dynamic depending of the test eenvironment (truffle in this case)
+            // por isso notEqual
+            assert.notEqual(address, 0x1, '-- not admin adress --> ' + address);
             return saleInstance.tokenContract();
         }).then(function (address) {
             assert.notEqual(address, 0x0, '-- not token contract adress');
             return saleInstance.tokenPrice();
         }).then(function (price) {
             assert.equal(price, tokenPrice , '-- token price not correct');
-
         })
+    });
 
+    it(':: should facilitate token buying :::', function () {
+        return DappToken.deployed().then(function(inst){
+            tokenInstance = inst;
+            return SugEtherSale.deployed();
+        }).then(function (inst) {
+            saleInstance = inst;
+
+            return tokenInstance.transfer(saleInstance.address, tokensAvailable, {from: admin});
+        }).then(function (receipt) {
+            //ETHER  = tokenNumber * wei price
+            let numbOfTokens = 10;
+            let value = numbOfTokens * tokenPrice;
+            return saleInstance.buyTokens(numbOfTokens, {from: buyer, value: value});
+        }).then(function (receipt) {
+            //testing receipt
+            assert.equal(receipt.logs.length, 1, '--- lenght is worng');
+            assert.equal(receipt.logs[0].event, 'Sell', '--- should be Sell event!');
+            assert.equal(receipt.logs[0].args._buyer, buyer, '--- Sell FROM is wrong!');
+            assert.equal(receipt.logs[0].args._amount, 10, '--- Sell amount tokens TO is wrong!');
+            //assert.equal(receipt.logs[0].args._value, 10 * tokenPrice, '--- Sell value is wrong!');
+            return saleInstance.tokensSold();
+        }).then(function (tokenSold) {
+            assert.equal(tokenSold.toNumber(), 10, '-- number of tokens sold not 10!!');
+
+            return tokenInstance.balanceOf(buyer);
+        }).then(function (balance) {
+            assert.equal(balance.toNumber(), 10, '-- buyer shouldnt habe this balance');
+
+            return tokenInstance.balanceOf(saleInstance.address);
+        }).then(function (balance) {
+            assert.equal(balance.toNumber(), tokensAvailable - 10, '-- number of tokens inbalance not corresponding should sold 10!!');
+
+            //try to buy tokens from the ether value
+            return saleInstance.buyTokens( 10, {from: buyer, value: 1} );
+        }).then(assert.fail).catch(function (error) {
+            assert(error.message.indexOf('revert') > -1, 'msg.value must equal the number of WEIs' );
+
+            //try to buy a number grater than available
+            return saleInstance.buyTokens(800000, {from: buyer, value: 10 * tokenPrice})
+        }).then(assert.fail).catch(function (error) {
+            assert(error.message.indexOf('revert') > -1, '-- cannot buy more tokens than available');
+        });
+    });
+
+
+    it('should test the end of the tokenSale', function () {
+        return DappToken.deployed().then(function(inst){
+            tokenInstance = inst;
+            return SugEtherSale.deployed();
+        }).then(function (inst) {
+            saleInstance = inst;
+
+            //try to end sale from account other than the admin
+            return saleInstance.endSale({from: buyer})
+        }).then(assert.fail).catch(function (error) {
+            assert(error.message.indexOf('revert') > -1, '-- nao se pode terminar o ICO por outra conta')
+            return saleInstance.endSale({from: admin})
+        }).then(function (receipt) {
+            return tokenInstance.balanceOf(admin);
+        }).then(function (balance) {
+            assert.equal(balance.toNumber(), 999990, '-- balance errado');
+
+            //check if token price has been reset on destruction
+            //in new versions you cant even chack destrocted things
+            // em novas veroes nao se pode chcar variaveis que estao destruidas
+            // return saleInstance.tokenPrice();
+        // }).then(function (price) {
+        //     assert.equal(price.toNumber(), 0, '-- prince must be reseted!')
+        })
     });
 
 
